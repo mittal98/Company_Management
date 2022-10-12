1151ce4c423701ec42c38a926474a6c0b8c4799a
Mittal Patel <mittal.patel@1Rintern.local>
2022-10-12 15:49:59 +0530

    company-routing

--- src/app/company/company-routing.module.ts
+++ src/app/company/company-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyFormComponent } from './company-form/company-form.component';
 import { CompanyComponent } from './company.component';
 
-const routes: Routes = [{ path: '', component: CompanyComponent
+const routes: Routes = [{
+  path: '', component: CompanyComponent,
+
+
+  children: [
+    {
+      path: '',
+      component: CompanyComponent
+    },
+    {
+      path: 'add',
+      component: CompanyFormComponent
+    },
+    {
+      path: 'edit/:id',
+      component: CompanyFormComponent
+    }
+  ]
+
 }];
 
 @NgModule({
